ab71b3c3a7afc7fc5f23f77b849fce695ecc165f
agusm <paula.margni@estudiantes.utec.edu.uy>
2026-09-25 11:07:17 -0300

    Agrega clase VariablesConstantes y organiza comentarios del módulo

---
+++ VariablesConstantes.java
@@ -0,0 +1,23 @@
+package holamundo;
+
+public class VariablesConstantes {
+
+    public static void main(String[] args) {
+        // Variables
+        String nombre = "ernesto";
+        System.out.println(nombre);
+
+        nombre = "pamela";
+        System.out.println(nombre);
+
+        var correo = "pame12@gmail.com";
+        System.out.println(correo);
+
+        var anio = 2024;
+        System.out.println(anio);
+
+        // Constantes
+        final String CORREO_OFICIAL = "Pamef@gmail.com";
+        System.out.println(CORREO_OFICIAL);
+    }
+}
